d78524792058896effb939938039bbd1979123b7
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2025-12-07 13:54:05 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,26 @@
+// sw.js
+const CACHE_NAME = 'eos-guide-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/pkg/eos_guide_wasm.js', // Fichier généré par trunk
+  '/pkg/eos_guide_wasm_bg.wasm' // Fichier généré par trunk
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Renvoie la réponse du cache ou fait une requête réseau
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
